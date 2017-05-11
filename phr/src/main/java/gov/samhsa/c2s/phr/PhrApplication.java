fc5e4ca5a1c45c1eb98898e4acc15104e039ca5e
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-05-11 18:43:08 -0400

    Create initial spring framework and configuration
    #13764 - As a developer, I will have implemented the Upload Clinical Document UI and integrated with backend

--- phr/src/main/java/gov/samhsa/c2s/phr/PhrApplication.java
+++ phr/src/main/java/gov/samhsa/c2s/phr/PhrApplication.java
@@ -2,8 +2,10 @@ package gov.samhsa.c2s.phr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhrApplication {
 
     public static void main(String[] args) {
