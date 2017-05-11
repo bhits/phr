19c2bf20312d7172bdbf0fd00230f3a6562033ba
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-05-11 18:03:20 -0400

    Create initial maven project
    #13764 - As a developer, I will have implemented the Upload Clinical Document UI and integrated with backend

---
+++ phr/src/main/java/gov/samhsa/c2s/phr/PhrApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.c2s.phr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrApplication.class, args);
+	}
+}
