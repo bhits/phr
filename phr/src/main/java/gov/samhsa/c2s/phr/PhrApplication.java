fd875e37f183d1447ac63efd7cdf88788405e56e
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-05-11 18:13:57 -0400

    Fix formatting in PhrApplication.java
    #13764 - As a developer, I will have implemented the Upload Clinical Document UI and integrated with backend

--- phr/src/main/java/gov/samhsa/c2s/phr/PhrApplication.java
+++ phr/src/main/java/gov/samhsa/c2s/phr/PhrApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhrApplication.class, args);
+    }
 }
