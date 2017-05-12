486761350a2cb26916d9c4af761485e35696d600
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-05-12 18:08:37 -0400

    Create mapping from UploadedDocument to UploadedDocumentInfoDto
    #13764 - As a developer, I will have implemented the Upload Clinical Document UI and integrated with backend

---
+++ phr/src/main/java/gov/samhsa/c2s/phr/service/mapping/UploadedDocumentToUploadedDocumentInfoDtoMap.java
@@ -0,0 +1,18 @@
+package gov.samhsa.c2s.phr.service.mapping;
+
+import gov.samhsa.c2s.phr.domain.UploadedDocument;
+import gov.samhsa.c2s.phr.service.dto.UploadedDocumentInfoDto;
+import org.modelmapper.PropertyMap;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UploadedDocumentToUploadedDocumentInfoDtoMap extends PropertyMap<UploadedDocument, UploadedDocumentInfoDto> {
+    @Override
+    protected void configure() {
+        map().setDocumentId(source.getDocumentId());
+        map().setDocumentFileName(source.getDocumentFileName());
+        map().setDocumentName(source.getDocumentName());
+        map().setDocumentContentType(source.getDocumentContentType());
+        map().setDocumentDescription(source.getDocumentDescription());
+    }
+}
