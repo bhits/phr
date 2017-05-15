bccb111febb36dc836868de46bf6cdb8ffbe296b
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-05-15 19:27:51 -0400

    Refactor UploadedDocumentRepository
    Details:
      - Add missing '@Repository' annotation to class
      - Add JpaSpecificationExecutor to list of interfaces which the repository class extends
    
    #13764 - As a developer, I will have implemented the Upload Clinical Document UI and integrated with backend

--- phr/src/main/java/gov/samhsa/c2s/phr/domain/UploadedDocumentRepository.java
+++ phr/src/main/java/gov/samhsa/c2s/phr/domain/UploadedDocumentRepository.java
@@ -1,9 +1,12 @@
 package gov.samhsa.c2s.phr.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface UploadedDocumentRepository extends JpaRepository<UploadedDocument, Long> {
+@Repository
+public interface UploadedDocumentRepository extends JpaSpecificationExecutor<UploadedDocument>, JpaRepository<UploadedDocument, Long> {
     List<UploadedDocument> findAllByPatientMrn(String patientMrn);
 }
