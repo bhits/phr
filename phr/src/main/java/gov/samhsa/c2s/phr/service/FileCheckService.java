da01c5f1e365b5c2957ab68e57387cc6bb4cbb12
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-05-16 22:38:38 -0400

    Fix javadoc comment formatting for isFileOversized method
    #13764 - As a developer, I will have implemented the Upload Clinical Document UI and integrated with backend

--- phr/src/main/java/gov/samhsa/c2s/phr/service/FileCheckService.java
+++ phr/src/main/java/gov/samhsa/c2s/phr/service/FileCheckService.java
@@ -7,7 +7,7 @@ public interface FileCheckService {
      * Checks to see if a file's size is over the configured maximum limit in bytes
      *
      * @param file - the file whose size is to be checked
-     * @return true if file size is over the max, false if file size is equal to or less than the max
+     * @return true if file size is over the max; false if file size is equal to or less than the max
      */
     boolean isFileOversized(MultipartFile file);
 }
