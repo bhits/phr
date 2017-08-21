8c815c3e8a438801604dcb4ede7d51a5df9adc04
burcakulug <burcak.ulug@feisystems.com>
2017-08-21 09:40:14 -0400

    Add Care Plan document type to PHR sample document types script
    #15348:As a developer, I will be able to use a CCD segmentation workflow to handle Plan Care document type

--- phr-db-sample/insert_document_type_codes.sql
+++ phr-db-sample/insert_document_type_codes.sql
@@ -1,3 +1,5 @@
+USE `phr`;
+
 INSERT INTO `document_type_code`
 (`code`,
 `code_system`,
@@ -10,3 +12,16 @@ VALUES
 '2.59',
 'LOINC',
 'Summary of episode note');
+
+INSERT INTO `document_type_code`
+(`code`,
+`code_system`,
+`code_system_version`,
+`code_system_name`,
+`display_name`)
+VALUES
+('52521-2',
+'2.16.840.1.113883.6.1',
+'2.56',
+'LOINC',
+'Overall Plan of Care/Advance Care Directives');
